ffbb9a0efb9fb81f5b52b496417dea24debacee8
0x0Megasus <0x00megasus@gmail.com>
2026-05-15 19:54:05 +0100

    Add aylux.site origin to Allowed CORS

--- server/main.js
+++ server/main.js
@@ -30,7 +30,7 @@ const normalizeOrigin = (origin = "") => {
 const allowedOrigins = [
   process.env.CLIENT_URL || "",
   "https://mega-video-downloader.vercel.app",
-  "https://aylux.site",
+  "https://www.aylux.site",
   "http://localhost:5173",
   "http://localhost:3000"
 ]
